d6dd3033a4dc5a78a980afe8f121c49fd9741447
Miguel Diaz <miguel.diaz@codescrum.com>
2014-10-28 16:42:16 -0500

    initial sinatra server running

---
+++ server.rb
@@ -0,0 +1,7 @@
+# Framework core
+require 'sinatra'
+# Framework extras, to use that 'sinatra-contrib' gem needs to be installed
+# this component allows me autoload a file when this has changed
+require 'sinatra/reloader'
+
+
