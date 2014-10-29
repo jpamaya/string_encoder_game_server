6a6442e26cc875f602e781278ade66ea7f83d5ee
Miguel Diaz <miguel.diaz@codescrum.com>
2014-10-29 11:55:40 -0500

    add all-vowel substitution

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -11,9 +11,17 @@ class Encoder
   # Reverses the word
   #
   # @param [String] a word
-  # @return [String] the same word
+  # @return [String] the reversed word
   def reverse(word)
     word.reverse
+  end
+
+  # Substitutes vowels by numbers
+  #
+  # @param [String] a word
+  # @return [String] the word with vowels substituted by numbers
+  def vowels_to_numbers(word)
+    word.gsub(/[aeiou]/, {a: '4', e: '3', i: '1', o: '0', u: '6'}.stringify_keys)
   end
 
 end
--- spec/encoder_spec.rb
+++ spec/encoder_spec.rb
@@ -14,4 +14,15 @@ describe Encoder do
     end
   end
 
+  describe '#vowels_to_numbers' do
+    it 'takes a word, and substitutes the vowels by numbers' do
+      expect(encoder.vowels_to_numbers('a')).to eq '4'
+      expect(encoder.vowels_to_numbers('e')).to eq '3'
+      expect(encoder.vowels_to_numbers('i')).to eq '1'
+      expect(encoder.vowels_to_numbers('o')).to eq '0'
+      expect(encoder.vowels_to_numbers('u')).to eq '6'
+      expect(encoder.vowels_to_numbers('murcielago')).to eq 'm6rc13l4g0'
+    end
+  end
+
 end
