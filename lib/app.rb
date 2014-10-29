4ecebb28b3e44f173fd75b6adb2a64867bad3729
Miguel Diaz <miguel.diaz@codescrum.com>
2014-10-29 10:52:28 -0500

    setup reloading on encoder file

--- lib/app.rb
+++ lib/app.rb
@@ -1,18 +1,28 @@
 require_relative '../config/boot'
+require_relative './encoder'
 
 require 'sinatra/base'
 require 'sinatra/reloader'
 
 class StringEncoderGameServer < Sinatra::Base
 
+  attr_accessor :requests
+  attr_accessor :encoder
+
+  def initialize
+    @encoder = Encoder.new
+    super
+  end
+
   # Enable sinatra reloader
   configure :development do
     register Sinatra::Reloader
+    also_reload File.expand_path('../encoder.rb', __FILE__)
   end
 
   # Initial endpoint
   get '/word' do
-
+    "Encoder is: #{@encoder}"
   end
 
 end
