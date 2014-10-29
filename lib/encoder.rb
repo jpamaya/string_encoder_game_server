041a2ae9e6cec5e73a3f5ec3cca5f6ebb1b0d531
Miguel Diaz <miguel.diaz@codescrum.com>
2014-10-29 11:29:56 -0500

    add the noop method

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -1,3 +1,9 @@
 class Encoder
-
+  # Does nothing to the inputted word
+  #
+  # @param [String] a word
+  # @return [String] the same word
+  def noop(word)
+    word
+  end
 end
--- spec/encoder_spec.rb
+++ spec/encoder_spec.rb
@@ -1,7 +1,13 @@
 describe Encoder do
 
-  it 'does nothing' do
+  let(:encoder){ Encoder.new }
 
+  describe '#noop' do
+    it 'takes a word, and does nothing to it' do
+      expect(encoder.noop('anyword')).to eq 'anyword'
+      expect(encoder.noop('AnyString')).to eq 'AnyString'
+      expect(encoder.noop('01AnyString89')).to eq '01AnyString89'
+    end
   end
 
 end
